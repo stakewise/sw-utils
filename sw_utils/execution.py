7d5031dc510aaf96915a5bbc8621e800e0950a02
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2022-12-08 20:59:52 +0200

    Refactor ipfs clients (#4)
    * refactor ipfs clients
    
    * Update version
    
    * Add ipfs multi upload client
    
    * Remove redundant self argument
    
    * Remove quorum for ipfs upload

--- sw_utils/execution.py
+++ sw_utils/execution.py
@@ -5,28 +5,17 @@ from web3.eth import AsyncEth
 from web3.middleware import async_geth_poa_middleware
 from web3.net import AsyncNet
 
-from sw_utils.common import Singleton
-
 logger = logging.getLogger(__name__)
 
 
-class ExecutionClient(metaclass=Singleton):
-    clients: dict[str, Web3] = {}
+def get_execution_client(endpoint: str, is_poa=False) -> Web3:
+    client = Web3(
+        Web3.AsyncHTTPProvider(endpoint),
+        modules={'eth': (AsyncEth,), 'net': AsyncNet},
+        middlewares=[],
+    )
 
-    def get_client(self, endpoint: str, is_poa=False) -> Web3:
-        if self.clients.get(endpoint):
-            return self.clients[endpoint]
-
-        client = Web3(
-            Web3.AsyncHTTPProvider(endpoint),
-            modules={'eth': (AsyncEth,), 'net': AsyncNet},
-            middlewares=[],
-        )
-        logger.warning('Web3 HTTP endpoint=%s', endpoint)
-
-        if is_poa:
-            client.middleware_onion.inject(async_geth_poa_middleware, layer=0)
-            logger.warning('Injected POA middleware')
-
-        self.clients[endpoint] = client
-        return client
+    if is_poa:
+        client.middleware_onion.inject(async_geth_poa_middleware, layer=0)
+        logger.info('Injected POA middleware')
+    return client
