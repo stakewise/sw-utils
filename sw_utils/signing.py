31131b520695f41580e5511b4db7b46d686672d9
cyc60 <avsysoev60@gmail.com>
2022-12-12 15:30:49 +0300

    Separate private functions

--- sw_utils/signing.py
+++ sw_utils/signing.py
@@ -14,7 +14,7 @@ from py_ecc.optimized_bls12_381.optimized_curve import (Z1, add, curve_order,
                                                         multiply)
 from py_ecc.utils import prime_field_inv
 
-from .ssz import Serializable, bytes4, bytes32, bytes48, bytes96, uint64
+from .ssz import Serializable, bytes4, bytes32, bytes48, uint64
 from .typings import Bytes32
 
 PRIME = curve_order
@@ -45,56 +45,8 @@ class DepositMessage(Serializable):
     ]
 
 
-class DepositData(Serializable):
-    fields = [
-        ('pubkey', bytes48),
-        ('withdrawal_credentials', bytes32),
-        ('amount', uint64),
-        ('signature', bytes96),
-    ]
-
-
 class VoluntaryExit(Serializable):
     fields = [('epoch', uint64), ('validator_index', uint64)]
-
-
-def compute_deposit_domain(fork_version: bytes) -> bytes:
-    """
-    Deposit-only `compute_domain`
-    """
-    if len(fork_version) != 4:
-        raise ValueError(f'Fork version should be in 4 bytes. Got {len(fork_version)}.')
-    domain_type = DOMAIN_DEPOSIT
-    fork_data_root = compute_deposit_fork_data_root(fork_version)
-    return domain_type + fork_data_root[:28]
-
-
-def compute_deposit_fork_data_root(current_version: bytes) -> bytes:
-    """
-    Return the appropriate ForkData root for a given deposit version.
-    """
-    genesis_validators_root = ZERO_BYTES32  # For deposit, it's fixed value
-    if len(current_version) != 4:
-        raise ValueError(f'Fork version should be in 4 bytes. Got {len(current_version)}.')
-    return ForkData(
-        current_version=current_version,
-        genesis_validators_root=genesis_validators_root,
-    ).hash_tree_root
-
-
-def compute_signing_root(ssz_object: Serializable, domain: bytes) -> bytes:
-    """
-    Return the signing root of an object by calculating the root of the object-domain tree.
-    The root is the hash tree root of:
-    https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/beacon-chain.md#signingdata
-    """
-    if len(domain) != 32:
-        raise ValueError(f'Domain should be in 32 bytes. Got {len(domain)}.')
-    domain_wrapped_object = SigningData(
-        object_root=ssz_object.hash_tree_root,
-        domain=domain,
-    )
-    return domain_wrapped_object.hash_tree_root
 
 
 def compute_deposit_message(
@@ -122,13 +74,13 @@ def is_valid_deposit_data_signature(
     fork_version: bytes,
 ) -> bool:
     """Checks whether deposit data is valid."""
-    domain = compute_deposit_domain(fork_version=fork_version)
+    domain = _compute_deposit_domain(fork_version=fork_version)
     deposit_message = DepositMessage(
         pubkey=public_key,
         withdrawal_credentials=withdrawal_credentials,
         amount=amount_gwei,
     )
-    return MilagroBlsVerify(public_key, compute_signing_root(deposit_message, domain), signature)
+    return MilagroBlsVerify(public_key, _compute_signing_root(deposit_message, domain), signature)
 
 
 def reconstruct_shared_bls_public_key(public_keys: Dict[int, BLSPubkey]) -> BLSPubkey:
@@ -175,3 +127,42 @@ def is_valid_exit_signature(
         return False
     voluntary_exit = VoluntaryExit(epoch=0, validator_index=validator_index)
     return MilagroBlsVerify(public_key, voluntary_exit.hash_tree_root, signature)
+
+
+def _compute_deposit_domain(fork_version: bytes) -> bytes:
+    """
+    Deposit-only `compute_domain`
+    """
+    if len(fork_version) != 4:
+        raise ValueError(f'Fork version should be in 4 bytes. Got {len(fork_version)}.')
+    domain_type = DOMAIN_DEPOSIT
+    fork_data_root = _compute_deposit_fork_data_root(fork_version)
+    return domain_type + fork_data_root[:28]
+
+
+def _compute_deposit_fork_data_root(current_version: bytes) -> bytes:
+    """
+    Return the appropriate ForkData root for a given deposit version.
+    """
+    genesis_validators_root = ZERO_BYTES32  # For deposit, it's fixed value
+    if len(current_version) != 4:
+        raise ValueError(f'Fork version should be in 4 bytes. Got {len(current_version)}.')
+    return ForkData(
+        current_version=current_version,
+        genesis_validators_root=genesis_validators_root,
+    ).hash_tree_root
+
+
+def _compute_signing_root(ssz_object: Serializable, domain: bytes) -> bytes:
+    """
+    Return the signing root of an object by calculating the root of the object-domain tree.
+    The root is the hash tree root of:
+    https://github.com/ethereum/consensus-specs/blob/dev/specs/phase0/beacon-chain.md#signingdata
+    """
+    if len(domain) != 32:
+        raise ValueError(f'Domain should be in 32 bytes. Got {len(domain)}.')
+    domain_wrapped_object = SigningData(
+        object_root=ssz_object.hash_tree_root,
+        domain=domain,
+    )
+    return domain_wrapped_object.hash_tree_root
