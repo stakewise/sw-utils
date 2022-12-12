206b2a1515da34bcfbf9260e093f27c678074547
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2022-12-12 09:28:29 +0400

    Add remove, cleanup to ipfs upload client

--- sw_utils/execution.py
+++ sw_utils/execution.py
@@ -1,19 +1,13 @@
 import logging
 
 from web3 import Web3
-from web3.eth import AsyncEth
 from web3.middleware import async_geth_poa_middleware
-from web3.net import AsyncNet
 
 logger = logging.getLogger(__name__)
 
 
 def get_execution_client(endpoint: str, is_poa=False) -> Web3:
-    client = Web3(
-        Web3.AsyncHTTPProvider(endpoint),
-        modules={'eth': (AsyncEth,), 'net': AsyncNet},
-        middlewares=[],
-    )
+    client = Web3(Web3.AsyncHTTPProvider(endpoint))
 
     if is_poa:
         client.middleware_onion.inject(async_geth_poa_middleware, layer=0)
