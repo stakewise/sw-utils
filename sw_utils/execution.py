d5893509f8e53d8c44895999de9bdf7fffa51647
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2022-12-12 11:35:18 +0400

    Revert execution client change

--- sw_utils/execution.py
+++ sw_utils/execution.py
@@ -1,13 +1,19 @@
 import logging
 
 from web3 import Web3
+from web3.eth import AsyncEth
 from web3.middleware import async_geth_poa_middleware
+from web3.net import AsyncNet
 
 logger = logging.getLogger(__name__)
 
 
 def get_execution_client(endpoint: str, is_poa=False) -> Web3:
-    client = Web3(Web3.AsyncHTTPProvider(endpoint))
+    client = Web3(
+        Web3.AsyncHTTPProvider(endpoint),
+        modules={'eth': (AsyncEth,), 'net': AsyncNet},
+        middlewares=[],
+    )
 
     if is_poa:
         client.middleware_onion.inject(async_geth_poa_middleware, layer=0)
