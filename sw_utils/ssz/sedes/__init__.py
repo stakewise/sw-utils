c5c71527d50a214888fd871f6f1da944f2049943
Alexander Sysoev <avsysoev60@gmail.com>
2022-12-12 19:39:17 +0300

    Sign (#7)
    * Add signing functions
    
    * Separate private functions
    
    * Update process_new_events check
    
    * Sync package version notation
    
    * Strict milagro-bls-binding version

---
+++ sw_utils/ssz/sedes/__init__.py
@@ -0,0 +1,8 @@
+from .base import BaseSedes
+from .basic import BasicSedes, ProperCompositeSedes
+from .byte import Byte, byte
+from .byte_vector import ByteVector, bytes1, bytes4, bytes32, bytes48, bytes96
+from .container import Container
+from .serializable import Serializable
+from .uint import UInt, uint8, uint16, uint32, uint64, uint128, uint256
+from .vector import Vector
---
+++ sw_utils/typings.py
@@ -0,0 +1,3 @@
+from typing import NewType
+
+Bytes32 = NewType('Bytes32', bytes)
