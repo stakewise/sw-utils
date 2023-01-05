0f6587e096a864d6d0f424c79de320e5efae5539
Alexander Sysoev <avsysoev60@gmail.com>
2023-01-05 11:22:57 +0300

    Make signing functions public (#12)
    * Make signing functions public
    
    * Update bandit

--- sw_utils/__init__.py
+++ sw_utils/__init__.py
@@ -5,6 +5,8 @@ from .event_scanner import EventProcessor, EventScanner
 from .execution import get_execution_client
 from .ipfs import (BaseUploadClient, IpfsFetchClient, IpfsMultiUploadClient,
                    IpfsUploadClient, PinataUploadClient)
-from .signing import (compute_deposit_message, get_eth1_withdrawal_credentials,
+from .signing import (DepositData, DepositMessage, compute_deposit_domain,
+                      compute_deposit_message, compute_signing_root,
+                      get_eth1_withdrawal_credentials,
                       get_exit_message_signing_root,
                       is_valid_deposit_data_signature, is_valid_exit_signature)
