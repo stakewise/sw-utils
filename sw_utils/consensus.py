91f19253fed83cc79089a93161a7aa6fd22047a5
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2023-01-27 20:05:09 +0200

    Consensus timeout (#21)
    * Fix consensus timeout
    
    * Update version
    
    * Fix timeout for consensus, ipfs

--- sw_utils/consensus.py
+++ sw_utils/consensus.py
@@ -1,5 +1,8 @@
 from enum import Enum
+from typing import Any, Dict
 
+from eth_typing import URI
+from web3._utils.request import async_json_make_get_request
 from web3.beacon import AsyncBeacon
 
 GET_VALIDATORS = '/eth/v1/beacon/states/{0}/validators{1}'
@@ -34,11 +37,23 @@ EXITED_STATUSES = [
 
 
 class ExtendedAsyncBeacon(AsyncBeacon):
+    def __init__(
+        self,
+        base_url: str,
+        timeout: int = 60
+    ) -> None:
+        super().__init__(base_url)
+        self.timeout = timeout
+
     async def get_validators_by_ids(
         self, validator_ids: list[str], state_id: str = 'head'
     ) -> dict:
         endpoint = GET_VALIDATORS.format(state_id, f"?id={'&id='.join(validator_ids)}")
         return await self._async_make_get_request(endpoint)
+
+    async def _async_make_get_request(self, endpoint_uri: str) -> Dict[str, Any]:
+        uri = URI(self.base_url + endpoint_uri)
+        return await async_json_make_get_request(uri, timeout=self.timeout)
 
 
 def get_consensus_client(endpoint: str) -> ExtendedAsyncBeacon:
