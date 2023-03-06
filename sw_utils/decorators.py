6834f41f08d4615eae7cead003efff50fde7bfa2
Evgeny Gusarov <evgeny@stakewise.io>
2023-03-06 21:51:51 +0300

    Add backoff_connection_errors

---
+++ sw_utils/decorators.py
@@ -0,0 +1,22 @@
+import aiohttp
+import backoff
+
+connection_errors = (
+    aiohttp.ClientConnectionError,
+    aiohttp.ClientConnectorError,
+    aiohttp.ServerConnectionError,
+)
+
+
+def backoff_connection_errors(
+        max_tries: int | None = None,
+        max_time: int | None = None,
+        **kwargs
+):
+    return backoff.on_exception(
+        backoff.expo,
+        connection_errors,
+        max_tries=max_tries,
+        max_time=max_time,
+        **kwargs
+    )
---
+++ sw_utils/tests/test_decorators.py
@@ -0,0 +1,46 @@
+from unittest.mock import Mock
+
+import aiohttp
+import pytest
+from aiohttp import ClientResponseError
+
+from sw_utils.decorators import backoff_connection_errors
+
+
+class TestBackoffConnectionErrors:
+    def test_bad_request_http_error(self):
+        call_count = 0
+
+        @backoff_connection_errors(max_tries=2, max_time=2)
+        def raise_bad_request_http_error():
+            nonlocal call_count
+            call_count += 1
+
+            # simulate aiohttp.ClientResponse.raise_for_status
+            raise ClientResponseError(
+                Mock(),
+                (Mock(), ),
+                status=400,
+                message='',
+                headers={},
+            )
+
+        with pytest.raises(ClientResponseError):
+            raise_bad_request_http_error()
+
+        assert call_count == 1
+
+    def test_timeout_error(self):
+        call_count = 0
+
+        @backoff_connection_errors(max_tries=2, max_time=1)
+        def raise_timeout_error():
+            nonlocal call_count
+            call_count += 1
+
+            raise aiohttp.ServerTimeoutError
+
+        with pytest.raises(aiohttp.ServerTimeoutError):
+            raise_timeout_error()
+
+        assert call_count == 2
