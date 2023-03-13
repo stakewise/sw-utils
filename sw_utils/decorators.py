20e435a55e7aee5b3e3cdb9195d315d5d063ba50
Evgeny Gusarov <evgeny@stakewise.io>
2023-03-13 18:41:22 +0300

    Add backoff_requests_errors

--- sw_utils/decorators.py
+++ sw_utils/decorators.py
@@ -2,6 +2,7 @@ import functools
 
 import aiohttp
 import backoff
+import requests
 
 
 class RecoverableServerError(Exception):
@@ -15,10 +16,10 @@ class RecoverableServerError(Exception):
         super().__init__()
 
 
-def wrap_500_errors(f):
+def wrap_aiohttp_500_errors(f):
     """
     Allows to distinguish between HTTP 400 and HTTP 500 errors.
-    Both are represented by `ClientResponseError`.
+    Both are represented by `aiohttp.ClientResponseError`.
     """
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
@@ -57,7 +58,57 @@ def backoff_aiohttp_errors(
         @functools.wraps(f)
         def wrapper(*args, **kwargs):
             try:
-                return backoff_decorator(wrap_500_errors(f))(*args, **kwargs)
+                return backoff_decorator(wrap_aiohttp_500_errors(f))(*args, **kwargs)
+            except RecoverableServerError as e:
+                raise e.origin
+
+        return wrapper
+    return decorator
+
+
+def wrap_requests_500_errors(f):
+    """
+    Allows to distinguish between HTTP 400 and HTTP 500 errors.
+    Both are represented by `requests.HTTPError`.
+    """
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except requests.HTTPError as e:
+            if e.response.status >= 500:
+                raise RecoverableServerError(e) from e
+            raise
+    return wrapper
+
+
+def backoff_requests_errors(
+        max_tries: int | None = None,
+        max_time: int | None = None,
+        **kwargs
+):
+    """
+    Retry:
+      * connection errors
+      * HTTP 500 errors
+    Do not retry:
+      * HTTP 400 errors
+      * regular Python errors
+    """
+
+    backoff_decorator = backoff.on_exception(
+        backoff.expo,
+        (requests.ConnectionError, requests.Timeout, RecoverableServerError),
+        max_tries=max_tries,
+        max_time=max_time,
+        **kwargs
+    )
+
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            try:
+                return backoff_decorator(wrap_requests_500_errors(f))(*args, **kwargs)
             except RecoverableServerError as e:
                 raise e.origin
 
