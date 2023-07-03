6c9e449b6fb407105d0672ba6e48dc21228edae2
cyc60 <avsysoev60@gmail.com>
2023-07-04 00:24:40 +0300

    Add support for fallback nodes

--- sw_utils/decorators.py
+++ sw_utils/decorators.py
@@ -6,32 +6,13 @@ import aiohttp
 import backoff
 import requests
 
+from sw_utils.exceptions import (
+    AiohttpRecoveredErrors,
+    RecoverableServerError,
+    RequestsRecoveredErrors,
+)
+
 logger = logging.getLogger(__name__)
-
-
-class RecoverableServerError(Exception):
-    """
-    Wrapper around ClientResponseError for HTTP 500 errors.
-    Only for internal use inside sw-utils library.
-    Do not raise `RecoverableServerError` in application code.
-    """
-
-    def __init__(self, origin: requests.HTTPError | aiohttp.ClientResponseError):
-        self.origin = origin
-        if isinstance(origin, requests.HTTPError):
-            self.status_code = origin.response.status_code
-            self.uri = origin.response.url
-        elif isinstance(origin, aiohttp.ClientResponseError):
-            self.status_code = origin.status
-            self.uri = origin.request_info
-
-        super().__init__()
-
-    def __str__(self):
-        return (
-            f'RecoverableServerError (status_code: {self.status_code}, '
-            f'uri: {self.uri}): {self.origin}'
-        )
 
 
 def wrap_aiohttp_500_errors(f):
@@ -72,7 +53,7 @@ def backoff_aiohttp_errors(max_tries: int | None = None, max_time: int | None = 
 
     backoff_decorator = backoff.on_exception(
         backoff.expo,
-        (aiohttp.ClientConnectionError, RecoverableServerError, asyncio.TimeoutError),
+        AiohttpRecoveredErrors,
         max_tries=max_tries,
         max_time=max_time,
         **kwargs,
@@ -137,7 +118,7 @@ def backoff_requests_errors(max_tries: int | None = None, max_time: int | None =
 
     backoff_decorator = backoff.on_exception(
         backoff.expo,
-        (requests.ConnectionError, requests.Timeout, RecoverableServerError),
+        RequestsRecoveredErrors,
         max_tries=max_tries,
         max_time=max_time,
         **kwargs,
--- sw_utils/execution.py
+++ sw_utils/execution.py
@@ -1,15 +1,68 @@
 import logging
+from typing import Any
 
-from web3 import AsyncHTTPProvider, AsyncWeb3
+from eth_typing import URI
+from web3 import AsyncWeb3
 from web3.eth import AsyncEth
 from web3.middleware import async_geth_poa_middleware
 from web3.net import AsyncNet
+from web3.providers.async_rpc import AsyncHTTPProvider
+from web3.types import RPCEndpoint, RPCResponse
+
+from sw_utils.exceptions import AiohttpRecoveredErrors
 
 logger = logging.getLogger(__name__)
 
 
-def get_execution_client(endpoint: str, is_poa=False, timeout=60) -> AsyncWeb3:
-    provider = AsyncHTTPProvider(endpoint, request_kwargs={'timeout': timeout})
+class ProtocolNotSupported(Exception):
+    """Supported protocols: http, https"""
+
+
+class ExtendedAsyncHTTPProvider(AsyncHTTPProvider):
+    """
+    Provider with support for fallback endpoints.
+    """
+
+    _providers: list[AsyncHTTPProvider] = []
+
+    def __init__(
+        self,
+        endpoint_urls: list[str],
+        request_kwargs: Any | None = None,
+    ):
+        logger.debug({'msg': 'Initialize MultiHTTPProvider'})
+        self._endpoint_urls = endpoint_urls
+        self._providers = []
+
+        if endpoint_urls:
+            self.endpoint_uri = URI(endpoint_urls[0])
+
+        for host_uri in endpoint_urls:
+            if host_uri.startswith('http'):
+                self._providers.append(AsyncHTTPProvider(host_uri, request_kwargs))
+            else:
+                protocol = host_uri.split('://')[0]
+                raise ProtocolNotSupported(f'Protocol "{protocol}" is not supported.')
+
+        super().__init__()
+
+    async def make_request(self, method: RPCEndpoint, params: Any) -> RPCResponse:
+        for i, provider in enumerate(self._providers):
+            try:
+                response = await provider.make_request(method, params)
+                return response
+            except AiohttpRecoveredErrors as error:
+                if i == len(self._providers) - 1:
+                    raise error
+                logger.error(error)
+
+        return {}
+
+
+def get_execution_client(endpoints: list[str], is_poa=False, timeout=60) -> AsyncWeb3:
+    provider = ExtendedAsyncHTTPProvider(
+        endpoint_urls=endpoints, request_kwargs={'timeout': timeout}
+    )
     client = AsyncWeb3(
         provider,
         modules={'eth': (AsyncEth,), 'net': AsyncNet},
