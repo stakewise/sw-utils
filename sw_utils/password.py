038f2985fae34fc1e350a25366267f023bdcbd0e
Alexander Sysoev <avsysoev60@gmail.com>
2023-07-28 20:05:13 +0300

    Add password function and safe decorator (#52)

---
+++ sw_utils/password.py
@@ -0,0 +1,22 @@
+import secrets
+import string
+
+SPECIAL_CHARS = '!@#$%^&*()_'
+
+
+def generate_password(length=20) -> str:
+    alphabet = string.ascii_letters + string.digits + SPECIAL_CHARS
+    lower_set = set(string.ascii_lowercase)
+    upper_set = set(string.ascii_uppercase)
+    digits_set = set(string.digits)
+    special_set = set(SPECIAL_CHARS)
+    while True:
+        password = [secrets.choice(alphabet) for _ in range(length)]
+        password_set = set(password)
+        if (
+            upper_set.intersection(password_set)
+            and lower_set.intersection(password_set)
+            and special_set.intersection(password_set)
+            and digits_set.intersection(password_set)
+        ):
+            return ''.join(password)
