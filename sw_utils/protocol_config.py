c68de644b4de9ab9b960c98c6719b09c7a636a7f
Alexander Sysoev <avsysoev60@gmail.com>
2024-01-31 13:49:12 +0300

    Move protocol config to sw_utils (#85)
    * Move protocol config to sw_utils
    
    * Add type hint

---
+++ sw_utils/protocol_config.py
@@ -0,0 +1,45 @@
+from sw_utils.typings import Oracle, ProtocolConfig
+
+
+async def build_protocol_config(
+    config_data: dict, rewards_threshold: int | None = None, validators_threshold: int | None = None
+) -> ProtocolConfig:
+    oracles = []
+    for oracle in config_data['oracles']:
+        oracles.append(
+            Oracle(
+                public_key=oracle['public_key'],
+                endpoints=oracle['endpoints'],
+            )
+        )
+
+    if rewards_threshold and not 1 <= rewards_threshold <= len(oracles):
+        raise ValueError('Invalid rewards threshold')
+
+    if validators_threshold and not 1 <= validators_threshold <= len(oracles):
+        raise ValueError('Invalid validators threshold')
+
+    public_keys = [oracle.public_key for oracle in oracles]
+    if len(public_keys) != len(set(public_keys)):
+        raise ValueError('Duplicate public keys in oracles config')
+
+    exit_signature_recover_threshold = config_data['exit_signature_recover_threshold']
+
+    if validators_threshold and exit_signature_recover_threshold > validators_threshold:
+        raise ValueError('Invalid exit signature threshold')
+
+    return ProtocolConfig(
+        oracles=oracles,
+        rewards_threshold=rewards_threshold,
+        validators_threshold=validators_threshold,
+        exit_signature_recover_threshold=exit_signature_recover_threshold,
+        supported_relays=config_data['supported_relays'],
+        vault_fee_max_bps=config_data['vault_max_fee'],
+        validator_min_active_epochs=config_data['validator_min_active_epochs'],
+        validators_exit_queued_assets_bps=config_data['validators_exit_queued_assets_bps'],
+        validators_approval_batch_limit=config_data['validators_approval_batch_limit'],
+        validators_exit_rotation_batch_limit=config_data['validators_exit_rotation_batch_limit'],
+        exit_signature_epoch=config_data['exit_signature_epoch'],
+        signature_validity_period=config_data['signature_validity_period'],
+        until_force_exit_epochs=config_data['until_force_exit_epochs'],
+    )
--- sw_utils/typings.py
+++ sw_utils/typings.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 from typing import NewType
 
-from eth_typing import BlockNumber
-from web3.types import Timestamp
+from eth_keys.datatypes import PublicKey
+from eth_typing import BlockNumber, HexStr
+from web3 import Web3
+from web3.types import ChecksumAddress, Timestamp
 
 Bytes32 = NewType('Bytes32', bytes)
 
@@ -19,3 +21,45 @@ class ChainHead:
     consensus_block: int
     execution_block: BlockNumber
     execution_ts: Timestamp
+
+
+@dataclass
+class Oracle:
+    endpoints: list[str]
+    public_key: HexStr
+
+    @property
+    def address(self) -> ChecksumAddress:
+        public_key = PublicKey(Web3.to_bytes(hexstr=self.public_key))
+        return public_key.to_checksum_address()
+
+
+@dataclass
+# pylint: disable-next=too-many-instance-attributes
+class ProtocolConfig:
+    oracles: list[Oracle]
+    supported_relays: list[str]
+
+    # 1 percent = 100 bps
+    vault_fee_max_bps: int
+
+    validator_min_active_epochs: int
+    # 1 percent = 100 bps
+    validators_exit_queued_assets_bps: int
+
+    validators_approval_batch_limit: int
+    validators_exit_rotation_batch_limit: int
+
+    # The epoch when exit signature params changed.
+    exit_signature_epoch: int
+
+    # Time to submit new signatures into the contract (seconds).
+    signature_validity_period: int
+
+    # Period for operators to rotate exit signatures
+    until_force_exit_epochs: int
+
+    exit_signature_recover_threshold: int
+
+    validators_threshold: int | None = None
+    rewards_threshold: int | None = None
