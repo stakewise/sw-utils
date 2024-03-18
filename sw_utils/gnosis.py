b962aa30591581832e1e2a69674445f175e7d1ed
cyc60 <avsysoev60@gmail.com>
2024-03-18 23:26:02 +0300

    Review fix

--- sw_utils/gnosis.py
+++ sw_utils/gnosis.py
@@ -1,15 +1,13 @@
-from web3 import Web3
 from web3.types import Wei
 
-WAD = Web3.to_wei(1, 'ether')
-MGNO_RATE = Web3.to_wei(32, 'ether')
+MGNO_RATE = 32
 
 
 def convert_to_gno(mgno_amount: Wei) -> Wei:
     """Converts mGNO to GNO."""
-    return Wei(mgno_amount * WAD // MGNO_RATE)
+    return Wei(mgno_amount // MGNO_RATE)
 
 
 def convert_to_mgno(gno_amount: Wei) -> Wei:
     """Converts GNO to mGNO."""
-    return Wei(gno_amount * MGNO_RATE // WAD)
+    return Wei(gno_amount * MGNO_RATE)
