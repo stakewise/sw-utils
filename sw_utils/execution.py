b72dd2e073242d3c614837ed14c7fde4f5eec903
cyc60 <avsysoev60@gmail.com>
2025-12-08 16:30:21 +0300

    Rm mypy ignore

--- sw_utils/execution.py
+++ sw_utils/execution.py
@@ -47,7 +47,7 @@ class ExtendedAsyncHTTPProvider(AsyncHTTPProvider):
         self.retry_timeout = retry_timeout
 
         if endpoint_urls:
-            self.endpoint_uri = URI(endpoint_urls[0])  # type: ignore
+            self.endpoint_uri = URI(endpoint_urls[0])
 
         for host_uri in endpoint_urls:
             if host_uri.startswith('http'):
