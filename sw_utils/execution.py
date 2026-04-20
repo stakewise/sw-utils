fce686dda55c5b300d77bb0f4ae556118b21bce4
cyc60 <avsysoev60@gmail.com>
2026-04-20 11:23:07 +0300

    Fix ordering

--- sw_utils/execution.py
+++ sw_utils/execution.py
@@ -197,13 +197,13 @@ def get_execution_client(
     retry_timeout: int = 0,
     use_cache: bool = False,
     jwt_secret: str | None = None,
-    headers: dict | None = None,
+    headers: dict[str, str] | None = None,
 ) -> AsyncWeb3:
     default_headers = {
         'Content-Type': 'application/json',
     }
     headers = headers or {}
-    headers = headers | default_headers
+    headers = default_headers | headers
 
     if jwt_secret:
         token = _create_jwt_auth_token(jwt_secret)
